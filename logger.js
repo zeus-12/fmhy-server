aaf00ca0923b5a6d92787762b24e126e03d2bf82
taskylizard <75871323+taskylizard@users.noreply.github.com>
2022-05-09 01:05:51 +0530

    (guides): add logger

---
+++ logger.js
@@ -0,0 +1,46 @@
+const { Webhook, MessageBuilder } = require("discord-webhook-node");
+const hook = new Webhook(process.env.WEBHOOK);
+
+hook.setUsername("Guide Logs");
+hook.setAvatar("https://fmhy.cf/assets/logo.png");
+
+function add(title, link, nsfw) {
+	const description = `
+    ${`\`\`\`diff\n+ ${title} - ${link} | NSFW: ${nsfw}\`\`\``}
+    `; // what a beautiful mess
+	const message = new MessageBuilder()
+		.setTitle("Added")
+		.setColor("#07c4e3")
+
+		.setDescription(description)
+		.setTimestamp();
+	logger.send(message); // Send the message
+}
+
+function remove(title, link, nsfw) {
+	const description = `
+    ${`\`\`\`diff\n- ${title} - ${link} | NSFW: ${nsfw}\`\`\``}
+    `; // what a beautiful mess
+	const message = new MessageBuilder()
+		.setTitle("Removed")
+		.setColor("#ff4a4a")
+
+		.setDescription(description)
+		.setTimestamp();
+	logger.send(message);
+}
+
+function update(title, link, nsfw) {
+	const description = `
+    ${`\`\`\`diff\n++ ${title} - ${link} | NSFW: ${nsfw}\`\`\``}
+    `;
+	const message = new MessageBuilder()
+		.setTitle("Updated")
+		.setColor("#aec5e8")
+
+		.setDescription(description)
+		.setTimestamp();
+	logger.send(message);
+}
+
+module.exports = { add, remove, update };
