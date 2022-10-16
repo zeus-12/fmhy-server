218c5d28679fbb69f2b4eecf9045d61d72c9df6c
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-10-17 00:50:54 +0530

    removed github shit

--- models/discordUser.js
+++
@@ -1,8 +0,0 @@
-const db = require("mongoose");
-
-const discordUserSchema = new db.Schema({
-	username: String,
-	userid: Number,
-});
-
-module.exports = mongoose.model("discordUser", discordUserSchema);
