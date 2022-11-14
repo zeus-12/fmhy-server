cd92753798e2e1b2cda72afcbcb595da796d687e
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-11-14 21:03:00 +0530

    update cors allowlist

--- helpers/allowList.js
+++ helpers/allowList.js
@@ -1,8 +1,8 @@
 //removed access to server from localhost, for dev uncomment this line.
 const allowList = [
+	"https://fmhy.ml",
 	"https://fmhy.cf",
 	"https://fmhy.tk",
-	"https://fmhy.pages.dev",
 	"https://freemediaheckyeah.ml",
 	"http://localhost:3000",
 	"http://localhost:5173",
