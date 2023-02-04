4f743013446783e687e924d5cb8fcdeeeb4b6066
Vishnu <vishnuvinod072@gmail.com>
2023-02-04 23:33:51 +0530

    feat: added link-search for search

--- src/routes/SearchRoute.ts
+++ src/routes/SearchRoute.ts
@@ -3,22 +3,26 @@ const router = express.Router();
 import Search from "../models/Search";
 
 router.get("/", async (req, res) => {
-	const query = req.query.q;
+	const query = req.query.q as string;
 	const page = req.query.page || 1;
 	const nsfw = req.query.nsfw === "true" ? true : false;
 
 	const ITEMS_PER_PAGE = 30;
 
+	const regex = new RegExp(query, "i");
+
 	try {
 		const results = await Search.find({
-			title: { $regex: query, $options: "i" },
+			$or: [{ title: regex }, { link: { $in: [regex] } }],
+			// 	title: { $regex: query, $options: "i" },
 			isNsfw: nsfw,
 		})
 			.skip((+page - 1) * ITEMS_PER_PAGE)
 			.limit(ITEMS_PER_PAGE);
 
 		const count = await Search.countDocuments({
-			title: { $regex: query, $options: "i" },
+			$or: [{ title: regex }, { link: { $in: [regex] } }],
+			// title: { $regex: query, $options: "i" },
 			isNsfw: nsfw,
 		});
 
