d92685344e7d96eca2358b6ace03ce0eb2dc1981
Vishnu <vishnuvinod072@gmail.com>
2023-04-18 23:32:04 +0530

    update route

--- src/config/AllowList.ts
+++ src/config/AllowList.ts
@@ -7,6 +7,7 @@ const ALLOW_LIST = [
 	"https://www.freemediaheckyeah.ml",
 	"http://localhost:3000",
 	"http://localhost:5173",
+	"http://localhost:5174",
 	"http://127.0.0.1:5173",
 	"https://fmhy-vite.vercel.app",
 ];
